acfe9c0fe9b820ab698b90b7f8cb19785b91b079
Frederique Hsu <frederique.hsu@outlook.com>
2023-05-27 19:18:07 +0000

    Restore the build.sh file.

---
+++ build.sh
@@ -0,0 +1,11 @@
+# File name     : build.sh
+# Description   : 
+#
+#
+#
+
+
+
+arm-linux-gnueabihf-g++ -std=c++11 sizet.cpp -o sizet
+
+arm-linux-gnueabihf-g++ -std=c++11 endianness.cpp -o endianness
