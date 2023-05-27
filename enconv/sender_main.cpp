ea2f38537ade935c6f74f0099386610698b4c6d0
Frederique Hsu <frederique.hsu@outlook.com>
2023-05-27 23:23:59 +0800

    Add the sender project.

---
+++ enconv/sender_main.cpp
@@ -0,0 +1,21 @@
+/*!
+ * 	\file	sender_main.cpp
+ * 	\brief	
+ *
+ */
+
+
+#include "sender.hpp"
+
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char* argv[])
+{
+	int fd = open("envconv.data", O_WRONLY | O_APPEND | O_CREAT, 0666);
+
+	for (int index = 1; index < argc; ++index)
+	{
+		WriteMessage(fd, argv[1]);
+	}
+}
